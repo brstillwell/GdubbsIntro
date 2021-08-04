c5defb479f7468b54d5565b905eed4fa6d606a8d
Brandon <Brandon.Stillwell@gmail.com>
2021-08-03 18:51:06 -0700

    index file

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path');
+const router = express.Router();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname+'/index.html'));
 })
 
+app.use('/', router);
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
