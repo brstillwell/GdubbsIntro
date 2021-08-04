21fe53616e9229fc7f495d3c97b7c666078c7dfc
Brandon <Brandon.Stillwell@gmail.com>
2021-08-03 19:00:23 -0700

    added comments

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
+// These are the created variables
 const express = require('express')
+const path = require('path');
 const app = express()
 const port = 3000
-const path = require('path');
 const router = express.Router();
 
+// path the index
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname+'/index.html'));
 })
 
+// tells express to use the router
 app.use('/', router);
+
+// tells app to listen on port
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
