9fcfc2ee6b22ba2a3b407146a042394078cc6824
Brandon <Brandon.Stillwell@gmail.com>
2021-09-29 00:10:07 -0700

    added post method for testing

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 // These are the created variables
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 const router = express.Router();
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
 // path the index
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname+'/index.html'));
+})
+
+// endpoint for the login
+router.post('/login', (req, res) => {
+    console.log(req.body)
+    res.send('POST request to the homepage')
 })
 
 // tells express to use the router
